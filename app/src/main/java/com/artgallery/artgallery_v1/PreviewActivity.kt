19853ce7989d230da2fb8960433bb60f36db6be8
tuanture <andreyabalandinsmr@yandex.ru>
2023-03-21 21:50:11 +0400

    init commit

---
+++ app/src/main/java/com/artgallery/artgallery_v1/PreviewActivity.kt
@@ -0,0 +1,18 @@
+package com.artgallery.artgallery_v1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+
+    fun main_intent(view: View){
+        val main_intention = Intent(this, MainActivity::class.java)
+        startActivity(main_intention)
+    }
+}

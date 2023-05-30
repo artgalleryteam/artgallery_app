0ccd1ab3afdcceeb2ae46ce9a511129365316a28
tuanture <andreybalandinsmr@yandex.ru>
2023-05-31 03:30:13 +0400

    home final not

--- app/src/main/java/com/artgallery/artgallery_v1/PreviewActivity.kt
+++ app/src/main/java/com/artgallery/artgallery_v1/PreviewActivity.kt
@@ -18,27 +18,6 @@ class PreviewActivity : AppCompatActivity() {
 
         val preview_button: ConstraintLayout = findViewById(R.id.preview_button)
         preview_button.setOnClickListener {
-            val storage = Firebase.storage
-            val storageRef = storage.reference
-            val picRef = storageRef.child("pics/pic1.jpg")
-
-            val newMetadata = storageMetadata {
-                setCustomMetadata("name", "Крестьянка")
-                setCustomMetadata("year", "1885")
-                setCustomMetadata("author", "Винсент Ван Гог")
-                setCustomMetadata("description", "Картина Винсента Ван Гога “Крестьянка с саженцами картофеля” была написана в 1885 году. На картине изображена крестьянка в поле, сажающая картофель. Картина выполнена в стиле постимпрессионизма и находится в музее Круллер-Мюллер в Оттерло (Нидерланды).")
-                setCustomMetadata("style", "Реализм")
-            }
-
-            picRef.updateMetadata(newMetadata).addOnSuccessListener {
-                // Handle success
-                // For example, display a message to the user indicating that the metadata update was successful
-                Toast.makeText(this, "Metadata update successful!", Toast.LENGTH_SHORT).show()
-            }.addOnFailureListener {
-                // Handle failure
-                // For example, show an error message to the user
-                Toast.makeText(this, "An error occurred while updating metadata.", Toast.LENGTH_SHORT).show()
-            }
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/artgallery/artgallery_v1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/artgallery/artgallery_v1/ui/home/HomeFragment.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
+import androidx.gridlayout.widget.GridLayout
 import androidx.lifecycle.ViewModelProvider
+import com.artgallery.artgallery_v1.R
 import com.artgallery.artgallery_v1.databinding.FragmentHomeBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class HomeFragment : Fragment() {
 
@@ -33,6 +44,50 @@ private var _binding: FragmentHomeBinding? = null
 //    }
     return root
   }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val storage = Firebase.storage
+        val listRef = storage.reference.child("pics")
+
+        listRef.listAll()
+            .addOnSuccessListener { listResult ->
+                val gridLayout: GridLayout = view.findViewById(R.id.home_pic_space)
+                gridLayout.columnCount = 2
+
+                val margin = (16 * resources.displayMetrics.density).toInt()
+                val imageSize = (170 * resources.displayMetrics.density).toInt()
+                val cornerRadius = (25 * resources.displayMetrics.density).toInt()
+                val columnSpacing = (7 * resources.displayMetrics.density).toInt()
+
+                listResult.items.forEach { item ->
+                    item.downloadUrl.addOnSuccessListener { uri ->
+                        val imageView = ImageView(context)
+                        imageView.id = View.generateViewId()
+
+                        val layoutParams = GridLayout.LayoutParams()
+                        layoutParams.width = imageSize
+                        layoutParams.height = imageSize
+                        layoutParams.setMargins(columnSpacing, margin, columnSpacing, 0)
+                        imageView.layoutParams = layoutParams
+
+                        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+
+                        gridLayout.addView(imageView)
+
+                        Glide.with(this)
+                            .load(uri)
+                            .transform(CenterCrop(), RoundedCorners(cornerRadius))
+                            .into(imageView)
+                    }
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, "An error occurred", Toast.LENGTH_SHORT).show()
+            }
+    }
 
 override fun onDestroyView() {
         super.onDestroyView()

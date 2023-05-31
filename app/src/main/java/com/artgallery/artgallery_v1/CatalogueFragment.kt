d889004869b93504b77dadb24dc38acd7e0dc263
tuanture <andreybalandinsmr@yandex.ru>
2023-05-31 04:28:04 +0400

    home, catalogue final

--- app/src/main/java/com/artgallery/artgallery_v1/CatalogueFragment.kt
+++ app/src/main/java/com/artgallery/artgallery_v1/CatalogueFragment.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.gridlayout.widget.GridLayout
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +43,49 @@ class CatalogueFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_catalogue, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val storage = Firebase.storage
+        val listRef = storage.reference.child("pics")
+
+        listRef.listAll()
+            .addOnSuccessListener { listResult ->
+                val gridLayout: GridLayout = view.findViewById(R.id.home_pic_space)
+                gridLayout.columnCount = 2
+
+                val margin = (16 * resources.displayMetrics.density).toInt()
+                val imageSize = (170 * resources.displayMetrics.density).toInt()
+                val cornerRadius = (25 * resources.displayMetrics.density).toInt()
+                val columnSpacing = (7 * resources.displayMetrics.density).toInt()
+
+                listResult.items.forEach { item ->
+                    item.downloadUrl.addOnSuccessListener { uri ->
+                        val imageView = ImageView(context)
+                        imageView.id = View.generateViewId()
+
+                        val layoutParams = GridLayout.LayoutParams()
+                        layoutParams.width = imageSize
+                        layoutParams.height = imageSize
+                        layoutParams.setMargins(columnSpacing, margin, columnSpacing, 0)
+                        imageView.layoutParams = layoutParams
+
+                        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+
+                        gridLayout.addView(imageView)
+
+                        Glide.with(this)
+                            .load(uri)
+                            .transform(CenterCrop(), RoundedCorners(cornerRadius))
+                            .into(imageView)
+                    }
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, "An error occurred", Toast.LENGTH_SHORT).show()
+            }
     }
 
     companion object {
--- app/src/main/java/com/artgallery/artgallery_v1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/artgallery/artgallery_v1/ui/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.artgallery.artgallery_v1.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
@@ -18,6 +20,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 
 class HomeFragment : Fragment() {
@@ -25,69 +28,63 @@ class HomeFragment : Fragment() {
 private var _binding: FragmentHomeBinding? = null
   // This property is only valid between onCreateView and
   // onDestroyView.
-  private val binding get() = _binding!!
+  private lateinit var imageView: ImageView
+    private lateinit var authorTextView: TextView
+    private lateinit var nameTextView: TextView
+    private lateinit var yearTextView: TextView
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-    _binding = FragmentHomeBinding.inflate(inflater, container, false)
-    val root: View = binding.root
-
-//    val textView: TextView = binding.signInText
-//    homeViewModel.text.observe(viewLifecycleOwner) {
-//      textView.text = it
-//    }
-    return root
-  }
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        imageView = view.findViewById(R.id.home_pic)
+        authorTextView = view.findViewById(R.id.author_text_view)
+        nameTextView = view.findViewById(R.id.name_text_view)
+        yearTextView = view.findViewById(R.id.year_text_view)
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val storage = Firebase.storage
-        val listRef = storage.reference.child("pics")
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference
+        val pathReference = storageRef.child("pics")
 
-        listRef.listAll()
-            .addOnSuccessListener { listResult ->
-                val gridLayout: GridLayout = view.findViewById(R.id.home_pic_space)
-                gridLayout.columnCount = 2
+        pathReference.listAll().addOnSuccessListener { listResult ->
+            // Get the list of items in the "pics" folder
+            val items = listResult.items
 
-                val margin = (16 * resources.displayMetrics.density).toInt()
-                val imageSize = (170 * resources.displayMetrics.density).toInt()
-                val cornerRadius = (25 * resources.displayMetrics.density).toInt()
-                val columnSpacing = (7 * resources.displayMetrics.density).toInt()
+            // Select a random item
+            val randomItem = items.random()
 
-                listResult.items.forEach { item ->
-                    item.downloadUrl.addOnSuccessListener { uri ->
-                        val imageView = ImageView(context)
-                        imageView.id = View.generateViewId()
+            // Get the download URL of the random item
+            randomItem.downloadUrl.addOnSuccessListener { uri ->
+                // Load the image using Glide
+                Glide.with(requireContext())
+                    .load(uri)
+                    .into(imageView)
 
-                        val layoutParams = GridLayout.LayoutParams()
-                        layoutParams.width = imageSize
-                        layoutParams.height = imageSize
-                        layoutParams.setMargins(columnSpacing, margin, columnSpacing, 0)
-                        imageView.layoutParams = layoutParams
+                // Get the custom metadata of the image
+                randomItem.metadata.addOnSuccessListener { storageMetadata ->
+                    val author = storageMetadata.getCustomMetadata("author") ?: "Unknown"
+                    val name = storageMetadata.getCustomMetadata("name") ?: "Unknown"
+                    val year = storageMetadata.getCustomMetadata("year") ?: "Unknown"
 
-                        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-
-                        gridLayout.addView(imageView)
-
-                        Glide.with(this)
-                            .load(uri)
-                            .transform(CenterCrop(), RoundedCorners(cornerRadius))
-                            .into(imageView)
-                    }
+                    // Display the custom metadata in the TextViews
+                    authorTextView.text = "$author"
+                    nameTextView.text = "$name"
+                    yearTextView.text = "$year год"
                 }
             }
-            .addOnFailureListener {
-                Toast.makeText(context, "An error occurred", Toast.LENGTH_SHORT).show()
-            }
+        }.addOnFailureListener {
+            // Display a Toast message
+            Toast.makeText(requireContext(), "Error downloading image", Toast.LENGTH_SHORT).show()
+        }
     }
+
 
 override fun onDestroyView() {
         super.onDestroyView()
